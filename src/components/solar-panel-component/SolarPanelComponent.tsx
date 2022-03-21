8b34e20ac2bf7df967a22743c27d4f8818658e1b
Sigbjørn Berdal <sigbbe@stud.ntnu.no>
2022-03-21 10:48:03 +0100

    feat(#3): suggestion for solar panel component

--- src/components/solar-panel-component/SolarPanelComponent.tsx
+++ src/components/solar-panel-component/SolarPanelComponent.tsx
@@ -1,51 +1,166 @@
+import { Box, Button, Stack } from '@chakra-ui/react';
 import { motion } from 'framer-motion';
-import React, { useState } from 'react';
-import cloud from '../assets/cloud.svg';
-import sun from '../assets/sun.svg';
+import React, { useEffect, useRef, useState } from 'react';
+import cloudIcon from '../../assets/cloudIcon.svg';
+import './SolarPanelComponent.css';
+
+interface Weather {
+    sun: boolean;
+    cloud: boolean;
+}
+
+function weatherUpdateToAnimation(
+    w1: Weather | undefined,
+    w2: Weather
+): string[] {
+    w1 = !w1 ? { sun: false, cloud: false } : w1;
+    let sunState = '';
+    let cloudState = '';
+    if (w1.sun && w2.sun) {
+        sunState = 'sun';
+    } else if (w2.sun) {
+        sunState = 'sunIn';
+    } else if (w1.sun) {
+        sunState = 'sunOut';
+    } else {
+        sunState = 'noSun';
+    }
+    if (w1.cloud && w2.cloud) {
+        cloudState = 'cloud';
+    } else if (w2.cloud) {
+        cloudState = 'cloudIn';
+    } else if (w1.cloud) {
+        cloudState = 'cloudOut';
+    } else {
+        cloudState = 'noCloud';
+    }
+    return [sunState, cloudState];
+}
+const Weather: React.FC<Weather> = (weather: Weather) => {
+    const containerStyles =
+        'w-96 h-96 relative flex flex-col justify-center align-middle bg-slate-400 overflow-hidden rounded-2xl m-auto';
+    const sunVariants = {
+        sunIn: {
+            rotate: [90, 270],
+            transition: {
+                duration: 2,
+            },
+        },
+        sunOut: {
+            rotate: [270, 450],
+            transition: {
+                duration: 2,
+            },
+        },
+        sun: {
+            rotate: [270, 270],
+            transition: {
+                duration: 0,
+                repeat: 0,
+            },
+        },
+        noSun: {
+            rotate: [90, 90],
+            transition: {
+                duration: 0,
+            },
+        },
+    };
+    const cloudVariants = {
+        cloudIn: {
+            x: ['-100%', '0%'],
+            transition: {
+                duration: 2,
+            },
+        },
+        cloudOut: {
+            x: ['0%', '100%'],
+            transition: {
+                duration: 2,
+            },
+        },
+        cloud: {
+            x: ['0%', '0%'],
+            transition: {
+                duration: 0,
+            },
+        },
+        noCloud: {
+            x: ['-100%', '-100%'],
+            transition: {
+                duration: 2,
+            },
+        },
+        // You can do whatever you want here, if you just want it to stop completely use `rotate: 0`
+    };
+    const prevWeatherRef = useRef<Weather>();
+    useEffect(() => {
+        //assign the ref's current value to the count Hook
+        prevWeatherRef.current = weather;
+    }, [weather]);
+
+    const [sunAnime, cloudAnime]: string[] = weatherUpdateToAnimation(
+        prevWeatherRef.current,
+        weather
+    );
+    console.log(sunAnime, cloudAnime);
+    return (
+        <Box className={containerStyles}>
+            <motion.div
+                className={'sun-container'}
+                animate={sunAnime}
+                style={{ transform: 'rotate(90deg)' }}
+                variants={sunVariants}>
+                <motion.div className={'sun'} />
+            </motion.div>
+            <motion.img
+                className={'cloud'}
+                src={cloudIcon}
+                animate={cloudAnime}
+                variants={cloudVariants}
+            />
+        </Box>
+    );
+};
 
 const SolarPanelComponent: React.FC = () => {
-    const [s, sS] = useState(0);
-    const variants = {
-        hidden: { opacity: 0 },
-        visible: { opacity: 1 },
+    const [weather, setWeather] = useState<Weather>({
+        sun: false,
+        cloud: false,
+    });
+    const updateWeather = (sun: boolean, cloud: boolean): void => {
+        setWeather({ sun, cloud });
     };
-    const show = [[0], [1], [0, 1]];
-    const images = [
-        { src: sun, style: {} },
-        { src: cloud, style: { top: '50px', left: '40px' } },
-    ];
-
     return (
-        <div
-            className={
-                'w-96 h-96 flex flex-col justify-center align-middle bg-slate-400'
-            }>
-            <motion.div animate={{ scale: 0.5 }}>
-                {show[s].map((img, i) => {
-                    return (
-                        <img
-                            src={images[img].src}
-                            className={'absolute'}
-                            style={images[img].style}
-                            key={i}
-                            alt={'alt' + i}
-                        />
-                    );
-                })}
-            </motion.div>
-            <div className="w-full h-full"></div>
-            <div className="justify-center align-middle flex">
-                <button className={'m-2 p-2'} onClick={() => sS(0)}>
-                    1
-                </button>
-                <button className={'m-2 p-2'} onClick={() => sS(1)}>
-                    2
-                </button>
-                <button className={'m-2 p-2'} onClick={() => sS(2)}>
-                    3
-                </button>
-            </div>
-        </div>
+        <>
+            <Weather {...weather} />
+            <Stack spacing={4} direction="row" align="center">
+                <Button
+                    colorScheme="teal"
+                    size="md"
+                    onClick={() => updateWeather(true, false)}>
+                    Sol :)
+                </Button>
+                <Button
+                    colorScheme="teal"
+                    size="md"
+                    onClick={() => updateWeather(false, true)}>
+                    Sky ://
+                </Button>
+                <Button
+                    colorScheme="teal"
+                    size="md"
+                    onClick={() => updateWeather(true, true)}>
+                    Sol og Sky ://))
+                </Button>
+                <Button
+                    colorScheme="teal"
+                    size="md"
+                    onClick={() => updateWeather(false, false)}>
+                    Ingenting xD
+                </Button>
+            </Stack>
+        </>
     );
 };
 
