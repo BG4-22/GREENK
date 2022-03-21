f8b563dfedb621f98c0b264570b41fee1d928e8a
Per Holte <phholte@stud.ntnu.no>
2022-03-21 10:29:33 +0100

    feat(#8) added video to homepage

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,8 +1,9 @@
-import { Text } from '@chakra-ui/react';
+import { AspectRatio, Text } from '@chakra-ui/react';
 import React from 'react';
 
 export interface HomePropsI {}
-import { Grid, GridItem } from '@chakra-ui/react';
+import { Grid, GridItem, Image } from '@chakra-ui/react';
+import NidarvollVideo from './Assets/Nidarvoll-video.mp4';
 
 const Home: React.FC<HomePropsI> = (props: HomePropsI) => {
     return (
@@ -15,8 +16,13 @@ const Home: React.FC<HomePropsI> = (props: HomePropsI) => {
                 <GridItem rowSpan={1} colSpan={1} bg="rgba(255,255,255,0.2)">
                     <Text>SOLCELLE</Text>
                 </GridItem>
-                <GridItem rowSpan={2} colSpan={2} bg="rgba(255,255,255,0.8)">
-                    <Text>VIDEO</Text>
+                <GridItem rowSpan={2} colSpan={2} bg="#C3E0B5">
+                    <Text fontSize="32px" color="#455B33" marginBottom="0px">
+                        NIDARVOLL GRØNN KIOSK
+                    </Text>
+                    <video width="540px" height="540px" autoPlay loop>
+                        <source src={NidarvollVideo} type="video/mp4"></source>
+                    </video>
                 </GridItem>
                 <GridItem rowSpan={1} colSpan={1} bg="rgba(255,255,255,0.2)">
                     <Text>SPILL</Text>
