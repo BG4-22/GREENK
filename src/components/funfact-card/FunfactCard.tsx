82787a639fd35aa49a8f3794d7567a6f4b892cb5
Sigbjørn Berdal <sigbbe@stud.ntnu.no>
2022-03-21 12:01:32 +0100

    feat(#3): remove usage of tailwind

--- src/components/funfact-card/FunfactCard.tsx
+++ src/components/funfact-card/FunfactCard.tsx
@@ -1,3 +1,4 @@
+import { Box, List, ListItem, Stack, Text } from '@chakra-ui/react';
 import React from 'react';
 import '../index.css';
 
@@ -13,32 +14,15 @@ const FunfactCard: React.FC<FunfactCardPropsI> = ({
     list,
 }: FunfactCardPropsI) => {
     const listItems = list.map((item, i) => (
-        <li key={`item-${item}`}>{item}</li>
+        <ListItem key={`item-${item}`}>{item}</ListItem>
     ));
-
-    const containerStyle =
-        'w-fit min-w-max max-w-xs sm flex flex-col border-2 m-2 p-4 px-8';
-    const listStyle = 'list-outside w-fit max-w-xs sm text-l';
-    const orderedListStyle = 'list-decimal font-';
-    const unOrderedListStyle = 'list-disc';
-    const titleStyle = 'border-b-2 font-bold ';
     return (
-        <div
-            className={containerStyle}
-            style={{ backgroundColor: '#f2f4fc', borderColor: '#9dbe98' }}>
-            <h1 className={titleStyle} style={{ borderColor: '#9dbe98' }}>
-                {title}
-            </h1>
-            {ordered ? (
-                <ol className={`${listStyle} ${orderedListStyle}`}>
-                    {listItems}
-                </ol>
-            ) : (
-                <ul className={`${listStyle} ${unOrderedListStyle}`}>
-                    {listItems}
-                </ul>
-            )}
-        </div>
+        <Box style={{ backgroundColor: '#f2f4fc', borderColor: '#9dbe98' }}>
+            <Stack>
+                <Text borderColor={'#9dbe98'}>{title}</Text>
+                <List styleType={ordered ? '' : 'none'}>{listItems}</List>
+            </Stack>
+        </Box>
     );
 };
 
