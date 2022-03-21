471b4b29d15d49a5dab2d08d33e624c9b14d3309
Sigbjørn Berdal <sigbbe@stud.ntnu.no>
2022-03-21 10:47:36 +0100

    feat(#3): added solarpanel component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Layout from './layout';
 import { Home, Statistics } from './pages';
+import SolarPanelComponent from './components/solar-panel-component';
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
                 <Route path="/" element={<Layout />}>
                     <Route index element={<Home />} />
                     <Route path="statistikk" element={<Statistics />} />
+                    <Route path="solcelle" element={<SolarPanelComponent />} />
                     <Route path="*" element={<h1>No match</h1>} />
                 </Route>
             </Routes>
