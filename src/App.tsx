e63a7dbad0d5502c6c36c9504a741a25d21b174b
Sigbjørn Berdal <sigbbe@stud.ntnu.no>
2022-03-25 12:21:12 +0100

    feat(#3): create solar panel page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import SolarPanelComponent from './components/solar-panel-component';
 import { Home, Statistics } from './pages';
+import SolarPanelPage from './pages/solarpanel';
 
 function App() {
     return (
@@ -9,7 +9,7 @@ function App() {
             <Route path="/">
                 <Route index element={<Home />} />
                 <Route path="statistikk" element={<Statistics />} />
-                <Route path="solcelle" element={<SolarPanelComponent />} />
+                <Route path="solcelle" element={<SolarPanelPage />} />
                 <Route path="*" element={<h1>No match</h1>} />
             </Route>
         </Routes>
