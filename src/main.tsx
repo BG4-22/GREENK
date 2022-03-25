f52b8c77d4cc794feb1948c164823f495e00e693
Sigbjørn Berdal <sigbbe@stud.ntnu.no>
2022-03-25 13:49:53 +0100

    feat(#3): import theme from styles/Theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,10 @@
-import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
-
-const theme = extendTheme({
-    colors: {
-        brand: {
-            50: '#44337A',
-            100: '#B794F4',
-            500: '#B794F4', // you need this
-        },
-    },
-});
+import theme from './styles/Theme';
 
 ReactDOM.render(
     <React.StrictMode>
