17e88b362a879b1d579b5c928b0ea2c6ef5e46ca
amw <ase.wilhelmsen@outlook.com>
2022-03-28 22:48:13 +0200

    feat(#21) startet på slideshow av stats

--- src/pages/stats/Statistics.tsx
+++ src/pages/stats/Statistics.tsx
@@ -1,16 +1,49 @@
 import { Text } from '@chakra-ui/react';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Diagram from '../../components/Diagram';
 import DiagramC from '../../components/DiagramC';
+//import { Slide } from 'react-slideshow-image';
 
 export interface StatisticsPropsI {}
 
 const Statistics: React.FC<StatisticsPropsI> = (props: StatisticsPropsI) => {
+    const [component, setComponent] = useState('diagram');
+
+    //const components = [<Diagram />, <DiagramC />];
+
+    function setComponentIndex() {
+        switch (component) {
+            case 'start':
+                return <Diagram />;
+            case 'playing':
+                return <DiagramC />;
+
+            default:
+                return null;
+        }
+    }
+
+    /*function setComponentIndex() {
+        let componentIndex = 0;
+        getComponent(componentIndex);
+        if(componentIndex > components.length){
+            componentIndex = 0; 
+        }
+        componentIndex++; 
+    }*/
+
+    /*function getComponent(componentIndex: number) {
+        return components[componentIndex];
+    }*/
+
+    useEffect(() => {
+        setComponentIndex();
+    });
+
     return (
         <>
+            <button onClick={setComponentIndex}>NEXT</button>
             <Text>Statistikk</Text>
-            <Diagram />
-            <DiagramC />
         </>
     );
 };
