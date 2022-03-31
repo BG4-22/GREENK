3b174d54220b468e8d7e336be8d1ae6570e5749a
amw <ase.wilhelmsen@outlook.com>
2022-03-31 11:03:22 +0200

    feat(#21): slideshow funker å trykke på

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Diagram from './components/Diagram';
+import DiagramC from './components/DiagramC';
 import Layout from './layout';
 import { Home, Statistics } from './pages';
 
 function App() {
+    const statComponents = [<Diagram />, <DiagramC />, <h1>HALLO</h1>];
+    const Stats = <Statistics>{statComponents}</Statistics>;
     return (
         <div className="App">
             <Routes>
                 <Route path="/" element={<Layout />}>
                     <Route index element={<Home />} />
-                    <Route path="statistikk" element={<Statistics />} />
+                    <Route path="statistikk" element={Stats} />
                     <Route path="*" element={<h1>No match</h1>} />
                 </Route>
             </Routes>
--- src/pages/stats/Statistics.tsx
+++ src/pages/stats/Statistics.tsx
@@ -4,29 +4,16 @@ import Diagram from '../../components/Diagram';
 import DiagramC from '../../components/DiagramC';
 import { motion, AnimatePresence } from 'framer-motion';
 import './stats.css';
+import { wrap } from 'popmotion';
 
-export interface StatisticsPropsI {}
+export interface StatisticsPropsI {
+    children: JSX.Element[];
+}
 
-const Statistics: React.FC<StatisticsPropsI> = (props: StatisticsPropsI) => {
+const Statistics: React.FC<StatisticsPropsI> = ({ children }) => {
     const [component, setComponent] = useState('diagram');
 
-    //const components = [<Diagram />, <DiagramC />];
-
-    /*function setComponentIndex() {
-        switch (component) {
-            case 'start':
-                return <Diagram />;
-            case 'playing':
-                return <DiagramC />;
-
-            default:
-                return null;
-        }
-    }*/
-
-    /*useEffect(() => {
-        setComponentIndex();
-    });*/
+    const statComponents = [<Diagram />, <DiagramC />];
 
     const variants = {
         enter: (direction: number) => {
@@ -55,39 +42,46 @@ const Statistics: React.FC<StatisticsPropsI> = (props: StatisticsPropsI) => {
     };
 
     const [[page, direction], setPage] = useState([0, 0]);
+
+    const componentIndex = wrap(0, children.length, page);
+
     const paginate = (newDirection: number) => {
         setPage([page + newDirection, newDirection]);
     };
+    console.log(page, componentIndex);
 
     return (
         <>
             <Text>Statistikk</Text>
             <AnimatePresence initial={false} custom={direction}>
-                <motion.img
-                    key={page}
-                    //src={images[imageIndex]}
-                    custom={direction}
-                    variants={variants}
-                    initial="enter"
-                    animate="center"
-                    exit="exit"
-                    transition={{
-                        x: { type: 'spring', stiffness: 300, damping: 30 },
-                        opacity: { duration: 0.2 },
-                    }}
-                    drag="x"
-                    dragConstraints={{ left: 0, right: 0 }}
-                    dragElastic={1}
-                    onDragEnd={(e, { offset, velocity }) => {
-                        const swipe = swipePower(offset.x, velocity.x);
+                <div className="statsWrapper">
+                    <motion.div
+                        key={page}
+                        //src={statComponents[componentIndex]}
+                        custom={direction}
+                        variants={variants}
+                        initial="enter"
+                        animate="center"
+                        exit="exit"
+                        transition={{
+                            x: { type: 'spring', stiffness: 300, damping: 30 },
+                            opacity: { duration: 0.2 },
+                        }}
+                        drag="x"
+                        dragConstraints={{ left: 0, right: 0 }}
+                        dragElastic={1}
+                        onDragEnd={(e, { offset, velocity }) => {
+                            const swipe = swipePower(offset.x, velocity.x);
 
-                        if (swipe < -swipeConfidenceThreshold) {
-                            paginate(1);
-                        } else if (swipe > swipeConfidenceThreshold) {
-                            paginate(-1);
-                        }
-                    }}
-                />
+                            if (swipe < -swipeConfidenceThreshold) {
+                                paginate(1);
+                            } else if (swipe > swipeConfidenceThreshold) {
+                                paginate(-1);
+                            }
+                        }}>
+                        {children[componentIndex]}
+                    </motion.div>
+                </div>
             </AnimatePresence>
             <div className="next" onClick={() => paginate(1)}>
                 {'‣'}
