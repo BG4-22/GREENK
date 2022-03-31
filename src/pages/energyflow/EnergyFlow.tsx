612041b48cea2db3f532613a900528ed5c218951
Per Holte <phholte@stud.ntnu.no>
2022-03-31 15:43:46 +0200

    feat(#24): Minor changes to the video

--- src/pages/energyflow/EnergyFlow.tsx
+++ src/pages/energyflow/EnergyFlow.tsx
@@ -1,5 +1,5 @@
 import { Center } from '@chakra-ui/react';
-import Energiflyt from '../../assets/Energiflyt.mp4';
+import Energiflyt from '../../assets/EnergiflytNidarvoll.mp4';
 
 const EnergyFlow: React.FC = () => {
     return (
