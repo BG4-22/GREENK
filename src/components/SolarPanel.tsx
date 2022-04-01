01e8536383fa5f0f28ad0e2186cd26fcc0a18a89
amw <ase.wilhelmsen@outlook.com>
2022-04-01 13:17:52 +0200

    feat(#25): solcellefigur er lagt til

--- src/components/SolarPanel.tsx
+++ src/components/SolarPanel.tsx
@@ -1,13 +1,15 @@
 import * as React from 'react';
 import { useEffect, useState } from 'react';
 import sunshine from '../img/sunshine.png';
+import solarPanel from '../img/Solar.png';
 
 export default function SolarPanel() {
     useEffect(() => {});
 
     return (
-        <div style={{ background: 'red', height: 'fit-content' }}>
+        <div>
             <img id="sunshine" src={sunshine}></img>
+            <img id="solar" src={solarPanel}></img>
         </div>
     );
 }
