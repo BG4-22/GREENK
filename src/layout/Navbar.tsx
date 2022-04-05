2ba2f36259eb5e981cd3d80c0de1814787a0230d
Per Holte <phholte@stud.ntnu.no>
2022-04-05 12:02:06 +0200

    feat(#33): Navbar complete

--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -5,99 +5,56 @@ import { Link } from 'react-router-dom';
 const Navbar: React.FC = () => {
     return (
         <Box
-            aria-label={'navigation'}
-            display={'flex'}
-            justify-content={'center'}
-            alignItems={'center'}
-            position={'relative'}
-            bg={'#6387ff'}
+            aria-label="navigation"
+            display="flex"
+            justify-content="space-around"
+            alignItems="center"
+            position="relative"
+            bg="#8BA5FF"
             borderRadius={70}
-            border={'.1px solid black'}
+            border=".1px solid black"
+            h={'75px'}
             w={'100%'}
-            h={'3rem'}
             maxW={'50%'}
-            margin={'2em auto auto auto'}
-            bottom={'1rem'}
             color="white">
-            <Grid h="100%" w="100%" templateColumns="1fr auto 1fr">
-                <GridItem h="100%" w="100%" alignSelf="center">
-                    <Button
-                        h="100%"
-                        bg={'#6387ff'}
-                        display={'flex'}
-                        flexDirection={'column'}
-                        borderRadius={70}
-                        w={'100%'}
-                        _hover={{
-                            bg: '#8BA5FF',
-                            border: '.1px solid #000',
-                            color: '#000',
-                        }}>
-                        <Link
-                            to="/energiflyt"
-                            style={{
-                                textDecoration: 'none',
-                            }}>
-                            Energiflyt
-                        </Link>
-                    </Button>
-                </GridItem>
-                <GridItem h="100%" w="100%">
-                    <Center w={'100px'}>
-                        <Link
-                            to="/"
-                            style={{
-                                textDecoration: 'none',
-                            }}>
-                            <Button
-                                id={'navbar-home-button'}
-                                position={'absolute'}
-                                left={'50%'}
-                                transform={'translateX(-50%)'}
-                                bottom={'0px'}
-                                h={'110px'}
-                                w={'110px'}
-                                zIndex={1}
-                                bg={'#6387ff'}
-                                minH={'100%'}
-                                border={'.1px solid black'}
-                                borderRadius={'1000px'}
-                                colorScheme="blue"
-                                aria-label="Home button"
-                                _hover={{ bg: '#8BA5FF' }}>
-                                <Box
-                                    justifyContent={'center'}
-                                    alignItems={'center'}
-                                    display={'flex'}
-                                    flexDirection={'column'}>
-                                    <FiHome size={30} title={'Hjem'} />
-                                    <Text>Hjem</Text>
-                                </Box>
-                            </Button>
-                        </Link>
-                    </Center>
-                </GridItem>
-                <GridItem h="100%" w="100%" alignSelf="center">
-                    <Link
-                        to="/statistikk"
-                        style={{
-                            textDecoration: 'none',
-                        }}>
-                        <Button
-                            h="100%"
-                            bg={'#6387ff'}
-                            borderRadius={70}
-                            w={'100%'}
-                            _hover={{
-                                bg: '#8BA5FF',
-                                border: '.1px solid #000',
-                                color: '#000',
-                            }}>
-                            Statistikk
-                        </Button>
-                    </Link>
-                </GridItem>
-            </Grid>
+            <Link to={'/energiflyt'}>
+                <Box
+                    display={'flex'}
+                    justifyContent={'center'}
+                    alignItems={'center'}
+                    height={'75px'}
+                    width={'310px'}
+                    borderRadius={'3rem'}>
+                    Energiflyt
+                </Box>
+            </Link>
+            <Link to={'/'}>
+                <Box
+                    display={'flex'}
+                    justifyContent={'center'}
+                    alignItems={'center'}
+                    flexDir={'column'}
+                    height={'100px'}
+                    marginBottom={'25px'}
+                    width={'100px'}
+                    borderRadius={'3rem'}
+                    border={'.1px solid black'}
+                    bg="#8BA5FF">
+                    <FiHome size={30} />
+                    <Text style={{ fontSize: '10px' }}>Home</Text>
+                </Box>
+            </Link>
+            <Link to={'/statistikk'}>
+                <Box
+                    display={'flex'}
+                    justifyContent={'center'}
+                    alignItems={'center'}
+                    height={'75px'}
+                    width={'310px'}
+                    borderRadius={'3rem'}>
+                    Statistikk
+                </Box>
+            </Link>
         </Box>
     );
 };
