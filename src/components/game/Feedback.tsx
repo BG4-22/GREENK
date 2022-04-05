ab3c89558d97ab615deda40c8608ee9e28f60868
Fredrik Skaug <fredskau@stud.ntnu.no>
2022-04-05 15:48:11 +0200

    ready for merge

--- src/components/game/Feedback.tsx
+++ src/components/game/Feedback.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Img, Text } from '@chakra-ui/react';
+import { Box, Button, Flex, Image, Text } from '@chakra-ui/react';
 import netflix from '../../assets/game/netflix.svg';
 import playstation from '../../Assets/game/playstation.png';
 import tesla from '../../Assets/game/tesla.png';
@@ -24,68 +24,66 @@ function Feedback(props: {
     const comperisons = [
         {
             verb: 'se',
-            description: 'på',
+            noun: 'på',
             img: netflix,
             kWh: 0.077,
-            tidsramme: 'timer',
+            timerange: 'timer',
         },
         {
             verb: 'spille',
-            description: 'på',
+            noun: 'på',
             img: playstation,
             kWh: 0.26,
-            tidsramme: 'timer',
+            timerange: 'timer',
         },
         {
             verb: 'kjøre',
-            description: 'med',
+            noun: 'med',
             img: tesla,
             kWh: 1.49,
-            tidsramme: 'mil',
+            timerange: 'mil',
         },
     ];
     const comp = comperisons[Math.floor(Math.random() * comperisons.length)];
     return (
         <>
             <Center fontFamily="Comic Neue">
-                <Box
+                <Flex
+                    justifyContent="center"
+                    display="flex"
+                    flexDirection="column"
                     width="50%"
                     height="35%"
                     backgroundColor="white"
-                    style={{
-                        position: 'absolute',
-                        top: '20%',
-                        borderRadius: '50px',
-                    }}>
-                    <Center>
-                        <Text fontSize="xl" margin={10} marginTop="10%">
-                            {prompts.biggest.description} bruker {difference}kWh
-                            mer i måneden enn {prompts.smallest.description}!
+                    position= 'absolute'
+                    top='20%'
+                    borderRadius='50px'>
+                    <Text fontSize="xl" margin={10} marginTop="5%">
+                        {prompts.biggest.description} bruker {difference}kWh mer
+                        i måneden enn {prompts.smallest.description}!
+                    </Text>
+                    <Flex justifyContent={'center'}>
+                        <Text display="flex" flexDirection="row" justifyContent="center" alignItems={'center'} fontSize="xl">
+                            Det tilsvarer å {comp.verb}{' '}
+                            {
+                                <Text color="#8BA5FF" fontSize="2xl" margin={3}>
+                                    {' '}
+                                    {(difference / comp.kWh).toFixed(0)}{' '}
+                                    {comp.timerange}{' '}
+                                </Text>
+                            }{' '}
+                            {comp.noun}
                         </Text>
-                    </Center>
-                    <br></br>
-                    <Center>
-                        <Text fontSize="xl">Det tilsvarer å {comp.verb}</Text>
-                        <Text color="#8BA5FF" fontSize="2xl" margin={3}>
-                            {' '}
-                            {(difference / comp.kWh).toFixed(0)}{' '}
-                            {comp.tidsramme}{' '}
-                        </Text>
-                        <Text fontSize="xl" marginRight={3}>
-                            {comp.description}
-                        </Text>
-                        <img src={comp.img} width="100px" />
-                    </Center>
-                </Box>
+                        <Image src={comp.img} width="100px" objectFit="contain" marginLeft="15px"/>
+                    </Flex>
+                </Flex>
                 <Button
                     position="absolute"
-                    style={{
-                        color: 'white',
-                        background: '#F6DC97',
-                        boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
-                        borderRadius: '50px',
-                        fontSize: '25px',
-                    }}
+                    color="white"
+                    background='#F6DC97'
+                    boxShadow='0px 4px 4px rgba(0, 0, 0, 0.25)'
+                    borderRadius='50px'
+                    fontSize='25px'
                     width="15%"
                     height="8%"
                     top="60%"
