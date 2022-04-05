34a01674c6a2faa9844d92c00d6c7b8147bff52f
Sigbjørn Berdal <sigbbe@stud.ntnu.no>
2022-04-05 13:53:32 +0200

    feat(#34): done refactoring, closes #34

--- src/components/statistics/EnergySources.tsx
+++ src/components/statistics/EnergySources.tsx
@@ -1,4 +1,13 @@
-import { Box, Image, List, ListItem, Text } from '@chakra-ui/react';
+import {
+    Box,
+    Flex,
+    HStack,
+    Image,
+    List,
+    ListItem,
+    Text,
+    VStack,
+} from '@chakra-ui/react';
 import { FC } from 'react';
 import Skole from '../../assets/stats/skole.svg';
 
@@ -57,82 +66,82 @@ const colors: string[] = [
     'rgba(255, 0, 0, .5)',
 ];
 
-const EnergyElement: FC<EnergySourceI[]> = (props) => {
-    console.log(props);
-    return <></>;
+interface EnergyElementsPropsI {
+    elements: EnergySourceI[];
+}
+
+const EnergyElements: FC<EnergyElementsPropsI> = ({
+    elements,
+}): JSX.Element => {
+    return (
+        <>
+            {elements.map((element, index) => {
+                return (
+                    <Box pos={'relative'} h={'100%'} w={`${element.amount}%`}>
+                        <Box h={'90%'} bg={colors[index]} />
+                        <Text textAlign={'center'} fontSize={'1.8rem'}>
+                            {`${element.amount}%`}
+                        </Text>
+                    </Box>
+                );
+            })}
+        </>
+    );
 };
 
-const EnergySources: FC<{}> = () => {
-    const energyElements = sources.map((element, index) => {
-        return (
-            <Box pos={'relative'} h={'100%'} w={`${element.amount}%`}>
-                <Box h={'90%'} bg={colors[index]} />
-                <Text textAlign={'center'} fontSize={'1.8rem'}>
-                    {`${element.amount}%`}
-                </Text>
-            </Box>
-        );
-    });
-
-    const energyInfoListElements = sources.map((element, index) => {
-        return (
-            <ListItem
-                display={'inline-flex'}
-                justifyContent={'flex-start'}
-                w={'100%'}
-                alignItems={'center'}>
-                <Box
-                    pos={'relative'}
-                    h={'2rem'}
-                    w={'2rem'}
-                    bg={colors[index]}
-                    marginRight={'2rem'}></Box>
-                <Box pos={'relative'}>{element.name}</Box>
-            </ListItem>
-        );
-    });
-
+const EnergyInfoList: FC<EnergyElementsPropsI> = ({ elements }) => {
     return (
-        <Box
-            pos={'relative'}
-            w={'100%'}
-            h={'100%'}
-            display={'flex'}
-            flexDir={'row'}>
-            <Box
-                display={'flex'}
-                justifyContent={'center'}
-                w={'100%'}
-                h={'100%'}>
-                <Box
-                    pos={'absolute'}
-                    w={'75%'}
-                    h={'100%'}
-                    justifyContent={'center'}
-                    display={'flex'}>
-                    <Image src={Skole} alt={'Nidarvoll skole'} w={'50%'} />
-                </Box>
-                <Box
-                    pos={'relative'}
-                    m={'auto'}
-                    h={'75%'}
-                    w={'75%'}
-                    display={'flex'}
-                    flexDir={'row'}>
-                    {energyElements}
-                </Box>
-            </Box>
-            <Box
-                pos={'relative'}
-                w={'60%'}
-                h={'100%'}
-                display={'flex'}
-                flexDir={'row'}
-                justifyContent={'center'}
-                alignItems={'center'}>
-                <List w={'90%'}>{energyInfoListElements}</List>
-            </Box>
-        </Box>
+        <List spacing={'1rem'} w={'90%'}>
+            {elements.map((element, index) => {
+                return (
+                    <ListItem
+                        key={`${element.name}-${index}`}
+                        display={'inline-flex'}
+                        justifyContent={'flex-start'}
+                        w={'100%'}
+                        alignItems={'center'}>
+                        <Box
+                            pos={'relative'}
+                            h={'2rem'}
+                            w={'2rem'}
+                            bg={colors[index]}
+                            marginRight={'2rem'}></Box>
+                        <Box pos={'relative'}>{element.name}</Box>
+                    </ListItem>
+                );
+            })}
+        </List>
+    );
+};
+
+const EnergySources: FC = () => {
+    return (
+        <>
+            <Text fontSize={'3rem'} transform={'translateY(2rem)'}>
+                Hvor kommer strømmen fra?
+            </Text>
+            <HStack pos={'relative'} w={'100%'} h={'100%'}>
+                <VStack justifyContent={'center'} w={'100%'} h={'100%'}>
+                    <Flex
+                        pos={'absolute'}
+                        w={'75%'}
+                        h={'100%'}
+                        justifyContent={'center'}>
+                        <Image src={Skole} alt={'Nidarvoll skole'} w={'50%'} />
+                    </Flex>
+                    <HStack spacing={0} h={'75%'} w={'75%'}>
+                        <EnergyElements elements={sources} />
+                    </HStack>
+                </VStack>
+                <HStack w={'60%'}>
+                    <EnergyInfoList elements={sources} />
+                </HStack>
+            </HStack>
+            <Text fontSize={'1.5rem'} transform={'translateY(-2rem)'}>
+                Til sammenlikning får den gjennomsnittlige skolen kjøpe 60% av
+                strømmen sin
+            </Text>
+        </>
     );
 };
 
--- src/pages/stats/Statistics.tsx
+++ src/pages/stats/Statistics.tsx
@@ -56,6 +56,7 @@ const Statistics: React.FC<StatisticsPropsI> = ({ children }) => {
                             height: '100%',
                             width: '100%',
                             display: 'flex',
+                            flexDirection: 'column',
                             alignItems: 'center',
                             justifyContent: 'center',
                         }}
