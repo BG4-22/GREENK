71940afe5e99eb1804090fde7a71773f04e06c6d
Sigbjørn Berdal <sigbbe@stud.ntnu.no>
2022-04-25 11:38:15 +0200

    added back other statistics pages

--- src/pages/Statistics.tsx
+++ src/pages/Statistics.tsx
@@ -1,13 +1,17 @@
 import Carousel from 'components/carousel/Carousel';
+import DiagramMoney from 'components/DiagramMoney';
 import EnergySources from 'components/statistics/EnergySources';
+import HeatPump from 'components/statistics/HeatPump';
+import OverallCon from 'components/statistics/OverallCon';
+import SolarPanel from 'components/statistics/SolarPanel';
 
 const Statistics: React.FC = () => {
     //Set the components that will be shown on the Statistics site
     const statComponents = [
-        // <OverallCon />,
-        // <HeatPump />,
-        // <SolarPanel />,
-        // <DiagramMoney />,
+        <OverallCon />,
+        <HeatPump />,
+        <SolarPanel />,
+        <DiagramMoney />,
         <EnergySources />,
     ].sort(() => (Math.random() > 0.5 ? 1 : -1));
     return <Carousel navButtons={'default'}>{statComponents}</Carousel>;
