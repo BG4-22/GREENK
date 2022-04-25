e2593193064014bcb23f5fe6f88bf3640bc0d3c1
Sigbjørn Berdal <sigbbe@stud.ntnu.no>
2022-04-25 11:30:47 +0200

    fix: use MockData.json in EnergySource.tsx and update slider buttons in home screen

--- src/pages/Statistics.tsx
+++ src/pages/Statistics.tsx
@@ -1,20 +1,16 @@
 import Carousel from 'components/carousel/Carousel';
-import DiagramMoney from 'components/DiagramMoney';
 import EnergySources from 'components/statistics/EnergySources';
-import HeatPump from 'components/statistics/HeatPump';
-import OverallCon from 'components/statistics/OverallCon';
-import SolarPanel from 'components/statistics/SolarPanel';
 
 const Statistics: React.FC = () => {
     //Set the components that will be shown on the Statistics site
     const statComponents = [
-        <OverallCon />,
-        <HeatPump />,
-        <SolarPanel />,
-        <DiagramMoney />,
+        // <OverallCon />,
+        // <HeatPump />,
+        // <SolarPanel />,
+        // <DiagramMoney />,
         <EnergySources />,
     ].sort(() => (Math.random() > 0.5 ? 1 : -1));
-    return <Carousel withButtons>{statComponents}</Carousel>;
+    return <Carousel navButtons={'default'}>{statComponents}</Carousel>;
 };
 
 export default Statistics;
