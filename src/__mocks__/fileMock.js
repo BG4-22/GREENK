eac339a537fa7ff9ef81dbf408ea9cbb5755a653
Per Holte <phholte@stud.ntnu.no>
2022-04-27 16:16:24 +0200

    fix(#75) - test mocks comments

--- src/__mocks__/fileMock.js
+++ src/__mocks__/fileMock.js
@@ -1,1 +1,6 @@
+/**
+ * This file is just a mock file for Jest to be re-directed to instead of handling static assets like stylesheets and images. 
+ * These files are not useful in tests, so we can mock them out.
+ */
+
 module.exports = 'stub-file';
