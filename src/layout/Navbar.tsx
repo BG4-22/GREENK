e5ec2addfccc17d2c2f69c6691754721d90312fd
Per Holte <phholte@stud.ntnu.no>
2022-04-27 16:16:03 +0200

    fix(#75) - Code comments App, Layout, Navbar

--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -3,6 +3,10 @@ import { FiHome } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
+/**
+ * Component that provides links to each url path. Decides what component that is shown.
+ * @returns Navbar
+ */
 const Navbar: React.FC = () => {
     return (
         <Box id={'navBar'} aria-label="navigation">
